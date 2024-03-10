9aebb28d54cee223eb529fd430829d74e3e4c4fa
AyoubAnej <alyapani9@gmail.com>
2024-03-10 19:40:37 +0000

    second commit

--- src/components/ListEmployeeComponents.jsx
+++ src/components/ListEmployeeComponents.jsx
@@ -1,30 +1,33 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { listEmployees } from "../services/EmployeeService";
+import { useNavigate } from "react-router-dom";
 
 function ListEmployeeComponents() {
-  const dummyData = [
-    {
-      id: 1,
-      firstName: "Ayoub",
-      lastName: "Boukhari",
-      email: "ayoubbou@gmail.com",
-    },
-    {
-      id: 2,
-      firstName: "Sofia",
-      lastName: "Rossi",
-      email: "sofiarossi@yahoo.it",
-    },
-    {
-      id: 3,
-      firstName: "Lucas",
-      lastName: "Hernandez",
-      email: "lucasher@hotmail.com",
-    },
-  ];
+  const [employees, setEmployees] = useState([]);
+
+  const navigator = useNavigate();
+
+  useEffect(() => {
+    listEmployees()
+      .then((response) => {
+        setEmployees(response.data);
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  }, []);
+
+  function addNewEmployee() {
+    navigator("/add-employee");
+  }
+
   return (
-    <div>
-      <h2>List of Employees</h2>
-      <table>
+    <div className="container">
+      <h2 className="text-center">List of Employees</h2>
+      <button className="btn btn-primary mb-2" onClick={addNewEmployee}>
+        Add Employee
+      </button>
+      <table className="table table-striped table-bordered">
         <thead>
           <tr>
             <th>Employee Id</th>
@@ -33,7 +36,16 @@ function ListEmployeeComponents() {
             <th>Employee Email</th>
           </tr>
         </thead>
-        <tbody></tbody>
+        <tbody>
+          {employees.map((employee) => (
+            <tr key={employee.id}>
+              <td>{employee.id}</td>
+              <td>{employee.firstName}</td>
+              <td>{employee.lastName}</td>
+              <td>{employee.email}</td>
+            </tr>
+          ))}
+        </tbody>
       </table>
     </div>
   );
